736579f70b2fa2a9b8994b5e47f61affdbc66d48
yanpeiting <449108802@qq.com>
2019-12-18 19:21:59 +0800

    修正2

--- src/views/Login/login.vue
+++ src/views/Login/login.vue
@@ -1,17 +1,44 @@
 <template>
   <div class="login-back">
-    <el-card class="box-card">
-    </el-card>
+    <div class="box-card">
+      <div class="top">
+        <img src="../../assets/logo_index.png" alt />
+      </div>
+      <el-form-item label="活动名称">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
+}
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
 .login-back {
   background-image: url("../../assets/login_bg.jpg");
+  background-size: cover;
   height: 100vh;
   display: flex;
   justify-content: center;
@@ -23,5 +50,14 @@ export default {}
   height: 346px;
   box-sizing: border-box;
   padding: 30px 0 30px 0;
+  background-color: white;
+  border-radius: 5px;
+  .top {
+    text-align: center;
+    padding: 0;
+    img {
+      height: 38px;
+    }
+  }
 }
 </style>
