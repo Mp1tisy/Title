e61b299c67c8263f697fefe8613f54860ee1be2e
yanpeiting <449108802@qq.com>
2019-12-18 20:09:03 +0800

    静态登录页面完成

--- src/views/Login/login.vue
+++ src/views/Login/login.vue
@@ -4,58 +4,17 @@
       <div class="top">
         <img src="../../assets/logo_index.png" alt />
       </div>
-      <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="活动名称">
-          <el-input v-model="form.name"></el-input>
-        </el-form-item>
-        <el-form-item label="活动区域">
-          <el-select v-model="form.region" placeholder="请选择活动区域">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="活动时间">
-          <el-col :span="11">
-            <el-date-picker
-              type="date"
-              placeholder="选择日期"
-              v-model="form.date1"
-              style="width: 100%;"
-            ></el-date-picker>
-          </el-col>
-          <el-col class="line" :span="2">-</el-col>
-          <el-col :span="11">
-            <el-time-picker
-              type="fixed-time"
-              placeholder="选择时间"
-              v-model="form.date2"
-              style="width: 100%;"
-            ></el-time-picker>
-          </el-col>
-        </el-form-item>
-        <el-form-item label="即时配送">
-          <el-switch v-model="form.delivery"></el-switch>
-        </el-form-item>
-        <el-form-item label="活动性质">
-          <el-checkbox-group v-model="form.type">
-            <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-            <el-checkbox label="地推活动" name="type"></el-checkbox>
-            <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-            <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-          </el-checkbox-group>
-        </el-form-item>
-        <el-form-item label="特殊资源">
-          <el-radio-group v-model="form.resource">
-            <el-radio label="线上品牌商赞助"></el-radio>
-            <el-radio label="线下场地免费"></el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item label="活动形式">
-          <el-input type="textarea" v-model="form.desc"></el-input>
+      <el-form ref="form">
+        <el-form-item>
+          <el-input style="width:351px;margin:0 auto;margin-top:25px;" placeholder="请输入手机号"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">立即创建</el-button>
-          <el-button>取消</el-button>
+          <el-input style="width:210px;" placeholder="验证码"></el-input>
+          <el-button plain style="float:right;height:40px;width:123px">发送验证码</el-button>
+        </el-form-item>
+        <el-checkbox v-model="checked"></el-checkbox><span class="report">我已阅读并同意<a class="check-choose">用户协议</a>和<a class="check-choose">隐私条款</a></span>
+        <el-form-item style="margin-top:20px;">
+          <el-button type="primary" style="width:100%">登录</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -66,21 +25,7 @@
 export default {
   data () {
     return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
-      }
-    }
-  },
-  methods: {
-    onSubmit () {
-      console.log('submit!')
+      checked: true
     }
   }
 }
@@ -100,7 +45,7 @@ export default {
   width: 431px;
   height: 346px;
   box-sizing: border-box;
-  padding: 30px 0 30px 0;
+  padding: 30px 40px 30px 40px;
   background-color: white;
   border-radius: 5px;
   .top {
@@ -110,5 +55,19 @@ export default {
       height: 38px;
     }
   }
+}
+
+.check-choose {
+  color: #3296fa;
+}
+
+.report {
+  font-size: 14px;
+  color: #999;
+  margin-left: 10px;
+}
+
+.el-form-item{
+  margin-bottom: 20px
 }
 </style>
