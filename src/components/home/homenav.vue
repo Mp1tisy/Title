fe4b6c5725ffc3880060967bf6887f150228d02e
yanpeiting <449108802@qq.com>
2019-12-19 20:47:38 +0800

    导航栏配置路由

--- src/components/home/homenav.vue
+++ src/components/home/homenav.vue
@@ -3,33 +3,33 @@
     <div class="logo">
       <img src="../../assets/logo_admin.png" alt />
     </div>
-    <el-menu background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b" style="width:201px;">
-      <el-menu-item>
+    <el-menu background-color="#353b4e" text-color="#adafb5" active-text-color="#ffd04b" style="width:201px;" router>
+      <el-menu-item index="/home">
         <span slot="title" style="margin-left:20px">首页</span>
       </el-menu-item>
-      <el-submenu index="1">
+      <el-submenu index="content">
         <template slot="title">
           <span slot="title" style="margin-left:20px">内容管理</span>
         </template>
         <el-menu-item-group>
-          <el-menu-item>发布文章</el-menu-item>
-          <el-menu-item>内容列表</el-menu-item>
-          <el-menu-item>评论列表</el-menu-item>
-          <el-menu-item>素材管理</el-menu-item>
+          <el-menu-item index="/home/publish">发布文章</el-menu-item>
+          <el-menu-item index="/home/articles">内容列表</el-menu-item>
+          <el-menu-item index="/home/comment">评论列表</el-menu-item>
+          <el-menu-item index="/home/material">素材管理</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
-      <el-submenu index="1">
+      <el-submenu index="fans">
         <template slot="title">
           <span slot="title" style="margin-left:20px">粉丝管理</span>
         </template>
         <el-menu-item-group>
-          <el-menu-item>图文数据</el-menu-item>
+          <el-menu-item index="/home/fans">图文数据</el-menu-item>
           <el-menu-item>粉丝概况</el-menu-item>
           <el-menu-item>粉丝画像</el-menu-item>
           <el-menu-item>粉丝列表</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
-      <el-menu-item>
+      <el-menu-item index="/home/account">
         <span slot="title" style="margin-left:20px">账户信息</span>
       </el-menu-item>
     </el-menu>
